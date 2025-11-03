9a29b119be9df5549208ac98c1f36a703cb961e8
manzidavidrw <manzi2020d@gmail.com>
2025-11-03 13:00:44 +0100

    adding the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
+    allowedHosts: ['kwetu-afri-market-96176.onrender.com'],
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
